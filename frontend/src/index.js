99f078d435bcc84f1c3e648e352b605a46840b55
Marisa <marisa.meurer@gmail.com>
2025-09-01 05:35:14 -0300

    Atualizar o index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Pode criar este arquivo vazio ou com estilo básico
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
