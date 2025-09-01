0dd7a2830a28b8cd5da9d39d9fe23b5cc4b0af77
Marisa <marisa.meurer@gmail.com>
2025-09-01 00:32:42 -0300

    Atualizar o index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/soma_aureum', {
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/soma_aureum';
+
+mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("MongoDB conectado!"))
-  .catch(err => console.log("Erro na conexão: ", err));
+})
+.then(() => console.log("MongoDB conectado!"))
+.catch(err => console.log("Erro na conexão: ", err));
