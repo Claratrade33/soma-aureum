d85c544f4a32a3863d0d3e4cafb7d728eb862ec2
Marisa <marisa.meurer@gmail.com>
2025-09-01 01:34:46 -0300

    Atualizar o App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,7 @@ import Dashboard from './components/Dashboard';
 import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default App;
