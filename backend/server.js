99df4b3e63aa7723c40774f4f568c8f4d8daf62a
Marisa <marisa.meurer@gmail.com>
2025-08-31 23:15:44 -0300

    Atualizar o server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('./db'); // conexão com MongoDB
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
