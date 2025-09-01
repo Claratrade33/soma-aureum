72f92b4cc901445d6f14189304b278e835ba7184
Marisa <marisa.meurer@gmail.com>
2025-09-01 19:52:01 -0300

    Criar errorHandler.js

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+export default (err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    error: {
+      message: err.message || 'Internal Server Error'
+    }
+  });
+};
